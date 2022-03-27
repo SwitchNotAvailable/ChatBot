fa6624930db6fec32dfec0210dedecd9306508c0
Okotel Emmanuel <enequeemmanuel.okotel@student.griffith.ie>
2022-03-27 21:01:22 +0000

    ChatBot Frame work

---
+++ ChatBot.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class ChatBot extends JFrame {
+
+    public ChatBot() {
+        JFrame jframe = new JFrame();
+        jframe.setTitle("CHATBOT");
+        jframe.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jframe.setLayout(null);
+        jframe.setVisible(true);
+        jframe.setResizable(false);
+        jframe.setSize(650,500);
+    }
+
+    public static void main(String[] args){
+        new ChatBot();
+
+    }
+}
