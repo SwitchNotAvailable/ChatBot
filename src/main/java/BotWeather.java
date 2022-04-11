ffe07487308aaa0d3c49c5b9a542ad1f38343036
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-04-11 15:34:01 +0100

    Fixed the weather api, and added rapid api

--- src/main/java/BotWeather.java
+++ src/main/java/BotWeather.java
@@ -2,8 +2,12 @@ package src.main.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,8 +24,29 @@ public class BotWeather { //Converting JSON into a Java map
 
     public static void main(String[] args) {
         String API_KEY = "387efdcc121d025a5cc6b12e335a4f0e";
-        String LOCATION = "london";
-        String urlString = "https://api.openweathermap.org/data/2.5/forecast/hourly?q=" + LOCATION + "&appid=" + API_KEY + "&units=metric";
+        String LOCATION = "London";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://community-open-weather-map.p.rapidapi.com/forecast?q=london"))
+                .header("X-RapidAPI-Host", "community-open-weather-map.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "2e3ec2e86fmsh248b52f6e7da633p1757f3jsn1560f7a8b1a0")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.body());
+
+
+        // https://api.openweathermap.org/data/2.5/weather?q=Somalia&appid=387efdcc121d025a5cc6b12e335a4f0e
+
+        String urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + LOCATION + "&appid=" + API_KEY + "&units=metric";
+        //String urlString = "https://api.openweathermap.org/data/2.5/forecast/hourly?q=" + LOCATION + "&appid=" + API_KEY; // + "&units=metric";
 
         try {
             StringBuilder result = new StringBuilder();
@@ -34,7 +59,7 @@ public class BotWeather { //Converting JSON into a Java map
                 result.append(line);
             }
             rd.close();
-            System.out.println(result);
+//            System.out.println(result);
 
             Map<String, Object> respMap = jsonToMap(result.toString());
             Map<String, Object> mainMap = jsonToMap(respMap.get("main").toString());
@@ -43,7 +68,7 @@ public class BotWeather { //Converting JSON into a Java map
             System.out.println("Current Temperature: " + mainMap.get("temp"));
             System.out.println("Current Humidity: " + mainMap.get("humidity"));
             System.out.println("Wind Speed: " + windMap.get("speed"));
-            System.out.println("Current Temperature");
+//            System.out.println("Current Temperature");
 
         }
 
